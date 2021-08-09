b803eb6d4ea30a53c2188e53774655df814fe4d5
Josue Flores <jos09901@tjx.com>
2021-08-09 10:25:07 -0400

    Completed Module

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,6 +1,14 @@
-function seed() {}
+function seed(a, b, c) {
+  return arguments[0,1,2];
+}
 
-function same([x, y], [j, k]) {}
+function same([x, y], [j, k]) {
+  if([x, y] === [j, k]){
+    return true;
+  } else{
+    return false;
+  }
+}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
 function contains(cell) {}
